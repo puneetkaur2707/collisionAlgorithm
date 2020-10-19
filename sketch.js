5b625f88ce7c2b38d1a2c22f1fe3d04a8d46f306
puneetkaur2707 <72548678+puneetkaur2707@users.noreply.github.com>
2020-10-19 16:21:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,16 +15,25 @@ function draw() {
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
 
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+  isTouching();
+  drawSprites();  
+}
+
+function isTouching()
+{
+    if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
       && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
       && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
-  }
-  else {
-    movingRect.shapeColor = "green";
-    fixedRect.shapeColor = "green";
-  }
-  drawSprites();
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) 
+    {
+    //movingRect.shapeColor = "red";
+    //fixedRect.shapeColor = "red";
+      return true;
+    }
+  else 
+   {
+   // movingRect.shapeColor = "green";
+    //fixedRect.shapeColor = "green";
+     return false;
+   }
 }
